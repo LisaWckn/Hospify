28741b38a3d820b7852b586fce87dfa22e4d91e4
Lisa Weickenmeier <lisaweickenmeier@outlook.de>
2024-05-17 10:26:59 +0200

    create component for header lines

---
+++ Hospify/src/app/components/header/header-lines/header-lines.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-lines',
+  templateUrl: './header-lines.component.html',
+  styleUrl: './header-lines.component.css'
+})
+export class HeaderLinesComponent {
+
+}
---
+++ Hospify/src/app/components/header/header-title/header-title.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-title',
+  templateUrl: './header-title.component.html',
+  styleUrl: './header-title.component.css'
+})
+export class HeaderTitleComponent {
+
+}
