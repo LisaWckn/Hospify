87b022759d19f77913097f0d23c01a0681c5d99b
Doma1985 <wellmanndw@gmx.de>
2024-07-26 14:50:27 +0200

    Hallo

--- Hospify/server.js
+++ Hospify/server.js
@@ -40,6 +40,48 @@ app.post('/executeQuery', async (req, res) => {
   }
 });
 
+app.post('/executePLSQLBlock', async (req, res) => {
+  const { bettID } = req.body;
+
+  const plsqlQuery = `
+    BEGIN
+      DECLARE
+        v_bettID NUMBER := :bettID;
+        v_einsatzbereit NUMBER;
+      BEGIN
+        SELECT einsatzbereit INTO v_einsatzbereit FROM bett WHERE bettID = v_bettID;
+
+        IF v_einsatzbereit = 1 THEN
+          INSERT INTO bett_log (bettID, status, timestamp)
+          VALUES (v_bettID, 'Einsatzbereit', SYSDATE);
+        ELSE
+          INSERT INTO bett_log (bettID, status, timestamp)
+          VALUES (v_bettID, 'Nicht einsatzbereit', SYSDATE);
+        END IF;
+      END;
+    END;
+  `;
+
+  let connection;
+
+  try {
+    connection = await oracledb.getConnection(dbConfig);
+    await connection.execute(plsqlQuery, { bettID: bettID });
+    await connection.commit();
+    res.send('PL/SQL block executed successfully');
+  } catch (err) {
+    res.status(500).send(err.message);
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error('Error closing connection:', err);
+      }
+    }
+  }
+});
+
 app.post('/executeInsert', async (req, res) => {
   const { query } = req.body;
   let connection;
